9f55846ada393e32ca5fbfaebe3dcab77f0676fd
MariiaLutska <myklush@ukr.net>
2022-06-15 00:16:41 +0300

    1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const imgGallery = document.querySelector('.gallery');
+console.log(imgGallery);
+
+const listImages = galleryItems
+    .map(item => `<div class="gallery">
+        <a class='gallery__item' href=${item.original}>
+        <img class='gallery__image'
+        src=${item.preview} alt='Image description' />
+        </a>
+        </div>`)
+    .join('');
+
+imgGallery.innerHTML = listImages;
+
+imgGallery.addEventListener('click', showModal);
+
+const lightbox = new SimpleLightbox('.gallery a', { captions: true, captionSelector: 'img', captionsData: 'alt', captionDelay: 250 });
+
+function showModal(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    console.log(event.target)
+    event.preventDefault();
+}
